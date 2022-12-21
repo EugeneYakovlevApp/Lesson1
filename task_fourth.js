23cd7f9eb7f9d64b042c65fc2a61e4bc30ee6ba4
Eugene Yakovlev <116457504+EugeneYakovlevApp@users.noreply.github.com>
2022-12-21 18:00:05 +0500

    add tasks for lesson5

---
+++ task_fourth.js
@@ -0,0 +1,33 @@
+const order = {
+    name: "Заказ",
+    description: "Суши по мексикански",
+    cost: 1,
+    curr: "EUR",
+    company: "Yazichok dostavka",
+    date: new Date("04.04.1922")
+}
+
+const unfilledOrder = {
+    description: "Суши по мексикански",
+    cost: 1,
+    curr: "EUR",
+}
+
+function infoAboutOrder(obj) {
+    validaton();
+    obj = setDefault(obj);
+    
+    function validaton(){
+        if (!obj.name || !obj.description || !obj.cost || obj.cost < 0 || !obj.curr || !obj.company || !obj.date){
+            console.log("Ошибка в данных");}else{
+        console.log("данные прошли проверку успешно");}
+    }
+}
+
+
+function setDefault({name="defName", description="defDesctiption",cost=2,curr="RUB",company="defCOmp",date=new Date("04.04.2000")}){
+    newObj = {
+        name,description,cost,curr,company,date
+    }
+    return newObj;
+}
