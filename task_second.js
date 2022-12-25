5b6e3e8a6799fb0678dd55bf71909e64e5ddb4e3
Eugene Yakovlev <116457504+EugeneYakovlevApp@users.noreply.github.com>
2022-12-25 20:46:33 +0500

    add for LEsson6

--- task_second.js
+++ task_second.js
@@ -1,43 +1,31 @@
-const firstCityMgn = {
-    'name':'Магнитогорск',
-    'date':new Date("01.01.1929"),
-    'population':'450000',
-    'country':'Россия',
-    showFullInformation
-}
-const secondCity17 = {
-    'name':'City 17',
-    'date':new Date('01.04.1800'),
-    'population':'400000',
-    'country':'Россия',
-    showFullInformation
+let array = [45, 78, 10, 3];
+function my(a, b) {
+    // console.log(a + b);
+    if (a < b) return -1;
+    if (a == b) return 0;
+    if (a > b) return 1;
 }
 
-const thirdCity = {}
-const fourthCity= {}
-    for(let entry in firstCityMgn){
-        thirdCity[entry] = firstCityMgn[entry];
+// function transform(array){
+//     console.log(this);
+// }
+
+Array.prototype.trans = function () {
+
+    console.log('Изначальный массив :');
+    for (let i = 0; i < this.length; i++) {
+        console.log(this[i]);
     }
-Object.assign(fourthCity,secondCity17);
 
-thirdCity.name = "Магадан";
-thirdCity.date = new Date("02.02.1982");
-
-fourthCity.name = "Новотроицк";
-fourthCity.date = new Date("03.03.1983");
-
-
-function showFullInformation(){
-    console.log("Город " + this.name + " был основан в "
-        + this.date
-        + " и расположен в стране " + this.country + ", численность населения города составляет " + this.population + " человек.");
-}
-
-showAllCitiesInformation();
-
-function showAllCitiesInformation(){
-    firstCityMgn.showFullInformation();
-    secondCity17.showFullInformation();
-    thirdCity.showFullInformation();
-    fourthCity.showFullInformation();
+    console.log('массив суммы цифр каждого числа :');
+    for (let i = 0; i < this.length; i++) {
+        let sum = 0;
+        while (this[i] > 0) {               
+            sum += this[i] % 10;
+            this[i] = Math.floor(this[i] / 10);
+            
+        }
+        this[i] = sum;
+        console.log(Math.round(sum));
+    }
 }
