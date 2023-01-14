c80cd68527d2d913fff1fde62f671c5f55e73ff3
Eugene Yakovlev <116457504+EugeneYakovlevApp@users.noreply.github.com>
2023-01-14 21:52:23 +0500

    code review Lesson6

--- task_second.js
+++ task_second.js
@@ -2,7 +2,7 @@ let array = [45, 78, 10, 3];
 function my(a, b) {
     // console.log(a + b);
     if (a < b) return -1;
-    if (a == b) return 0;
+    if (a === b) return 0;
     if (a > b) return 1;
 }
 
@@ -11,21 +11,24 @@ function my(a, b) {
 // }
 
 Array.prototype.trans = function () {
-
     console.log('Изначальный массив :');
+
     for (let i = 0; i < this.length; i++) {
         console.log(this[i]);
     }
 
     console.log('массив суммы цифр каждого числа :');
+
     for (let i = 0; i < this.length; i++) {
         let sum = 0;
-        while (this[i] > 0) {               
+        while (this[i] > 0) {
             sum += this[i] % 10;
             this[i] = Math.floor(this[i] / 10);
-            
+
         }
         this[i] = sum;
+        
         console.log(Math.round(sum));
+
     }
 }
