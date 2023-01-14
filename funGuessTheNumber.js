dee3c7be234453302ee776cf6e520f85d4881465
Eugene Yakovlev <116457504+EugeneYakovlevApp@users.noreply.github.com>
2023-01-14 21:11:03 +0500

    code review Lesson3

--- funGuessTheNumber.js
+++ funGuessTheNumber.js
@@ -4,46 +4,52 @@ let tryCount = 5;
 let randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
 
 game();
-function game(){
+
+function game() {
     let result;
-    while (tryCount > 0){
+
+    while (tryCount > 0) {
         console.log(randomNumber);
-        result = checkNumber(numLess,numMore,numWn,numLoss, mess(tryCount));     
-        tryCount -= 1;  
-        if(result == true){
+
+        result = checkNumber(numLess, numMore, numWn, numLoss, mess(tryCount));
+        tryCount -= 1;
+
+        if (result) {
             break;
         }
     }
-    if(result == false){ 
-    numLoss();}
+    if (!result) {
+        numLoss();
+    }
 }
 
-function checkNumber(numLess,numMore,numWn,numLoss,m){
-    if(m > randomNumber){
+function checkNumber(numLess, numMore, numWn, numLoss, m) {
+    if (m > randomNumber) {
         numMore();
-    }else if (m < randomNumber){
+    } else if (m < randomNumber) {
         numLess();
-    }else if ( m == randomNumber){
+    } else if (m === randomNumber) {
         numWn();
         return true;
     }
+
     return false;
 }
 
-function mess(tryCount){
-    return prompt("Угадай число! Кол-во попыток " + tryCount +". Введите ваше число: ", "");
+function mess(tryCount) {
+    return prompt("Угадай число! Кол-во попыток " + tryCount + ". Введите ваше число: ", "");
 }
 
-function numLess(){
+function numLess() {
     alert("Загаданное число больше");
 }
-function numMore(){
+function numMore() {
     alert("Загаданное число меньше");
 }
-function numWn(){
+function numWn() {
     alert("Вы отгадали число!");
 }
-function numLoss(){
+function numLoss() {
     alert("Вы проиграли! Количество попыток закончилось...");
 }
 
--- funIsEven.js
+++ funIsEven.js
@@ -1,5 +1,5 @@
 "use strict";
 
 const isEven = number => {
-    return number % 2 == 0 ? true : false;
+    return number % 2 === 0;
 }
