983287c8ca49ff77bf7f0e2229a0a2097e015ddd
Eugene Yakovlev <116457504+EugeneYakovlevApp@users.noreply.github.com>
2023-01-14 21:01:27 +0500

    code review

--- script.js
+++ script.js
@@ -2,16 +2,14 @@
 
 let userFirstName = "Genadii";
 let userLastName = "Bebureshvilli";
-let userBirthDay;
-let userBirthMonth;
-let userBirthYear;
 let userAge;
 
-userBirthDay = prompt("Введите день рождения", "");
-userBirthMonth = prompt("Введите месяц рождения", "");
-userBirthYear = prompt("Введите год рождения", "");
+let userBirthDay = prompt("Введите день рождения", "");
+let userBirthMonth = prompt("Введите месяц рождения", "");
+let userBirthYear = prompt("Введите год рождения", "");
 
-userAge = 2022 - Number(userBirthYear);
+userAge = 2022 - Number(userBirthYear); 
+
 alert("Фамилия : " + userLastName + " \n" 
       + "Имя : "  + userFirstName + " \n"
       + "Дата рождения : " + userBirthDay 
